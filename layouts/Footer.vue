6e959ac7b912d367f3bdf35524b2ce90ca99b346
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2024-03-21 10:21:38 +0000

    feat: add main layout

---
+++ layouts/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="FooterMain" class="w-full bg-[#e8e8e8] mt-10 mb-2">
+        <div class="max-w-[1200px] px-4 mx-auto">
+            <div class="md:flex gap-4 justify-between w-full">
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">Help</div>
+                    <div class="text-[#999999] text-sm">
+                        Help Center, Disputes & Reports, Buyer Protection,
+                        Report IPR infringement, Regulated Information
+                    </div>
+                    <div class="md:w-[50%] pt-4">
+                        <div class="text-lg font-semibold text-[#282828]">
+                            WakSunari Group
+                        </div>
+                        <div class="text-[#999999] text-sm">
+                            Indonesian, English
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="md:flex gap-4 justify-between w-full">
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">
+                        Telusuri berdasarkan Category
+                    </div>
+                    <div class="text-[#999999] text-sm">
+                        Semua Populer, Produk, Promosi, Harga Murah, Penilaian
+                        Baik, Tinjauan
+                    </div>
+                    <div class="md:w-[50%] pt-4">
+                        <div class="text-lg font-semibold text-[#282828]">
+                            WakSunari Group
+                        </div>
+                        <div class="text-[#999999] text-sm">
+                            WakSunari Group Website, WakSunariExpress
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+        <div class="max-w-[1200px] px-4 mx-auto">Copyright © 2024</div>
+    </div>
+</template>
